77639c6be66cfed8c3484f40780d4a302ff52d45
mahamfathy <mahafathyh@gmail.com>
2024-07-12 17:23:59 +0300

    ASCII data type

---
+++ data-type-ASCII.cpp
@@ -0,0 +1,12 @@
+// ASCII => American Standard Code for Information Interchange
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << int('a') << endl;
+    cout << int('A') << endl;
+    cout << char(70) << endl;
+    // cout << int("a") << endl; //error
+    return 0;
+}
