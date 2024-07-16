0906cee399639d1547d50916b6550ef1b076dc02
mahamfathy <mahafathyh@gmail.com>
2024-07-16 23:17:49 +0300

    watermelon problem

---
+++ task-day2/Watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    cout << (w % 2 == 0 && w > 2 ? "YES" : "NO") << endl;
+    // or //
+    // if (w % 2 == 0 && w > 2)
+    //     cout << "YES" << endl;
+    // else
+    //     cout << "NO" << endl;
+    return 0;
+}
