1466e53ad305c39be63d548a5d2a73838f09ce00
mahamfathy <mahafathyh@gmail.com>
2024-07-16 23:50:46 +0300

    max number

---
+++ task-day2/+-x-max-num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int add = a + b;
+    int sub = a - b;
+    int mult = a * b;
+    // int max_val = (add >= sub && add >= mult) ? add : (sub >= add && sub >= mult) ? sub
+    //                                                                           : mult;
+    // cout << max_val << endl;
+    if (add >= sub && add >= mult)
+    {
+        cout << add << endl;
+    }
+    else if (sub >= add && sub >= mult)
+    {
+        cout << sub << endl;
+    }
+    else
+    {
+        cout << mult << endl;
+    }
+    return 0;
+}
