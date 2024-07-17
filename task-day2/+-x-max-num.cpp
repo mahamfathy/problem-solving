44a293c048b5c32f0b563219f864956688449106
mahamfathy <mahafathyh@gmail.com>
2024-07-17 15:19:10 +0300

    ternary operator for max num

--- task-day2/+-x-max-num.cpp
+++ task-day2/+-x-max-num.cpp
@@ -10,17 +10,18 @@ int main()
     // int max_val = (add >= sub && add >= mult) ? add : (sub >= add && sub >= mult) ? sub
     //                                                                           : mult;
     // cout << max_val << endl;
-    if (add >= sub && add >= mult)
-    {
-        cout << add << endl;
-    }
-    else if (sub >= add && sub >= mult)
-    {
-        cout << sub << endl;
-    }
-    else
-    {
-        cout << mult << endl;
-    }
+    // if (add >= sub && add >= mult)
+    // {
+    //     cout << add << endl;
+    // }
+    // else if (sub >= add && sub >= mult)
+    // {
+    //     cout << sub << endl;
+    // }
+    // else
+    // {
+    //     cout << mult << endl;
+    // }
+    cout << (add >= sub && add >= mult ? add : (sub >= add && sub >= mult ? sub : mult)) << endl;
     return 0;
 }
