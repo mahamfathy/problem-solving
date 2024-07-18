199432d7b66c4ea0c3c91e88935a9e7b0317d583
mahamfathy <mahafathyh@gmail.com>
2024-07-18 23:48:57 +0300

    even and odds solution

--- task-day2/even-odds.cpp
+++ task-day2/even-odds.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int n, k;
-    ;
+    long long n, k;
     cin >> n >> k;
+
+    // Calculate the count of odd numbers
+    long long count_of_odd = (n + 1) / 2;
+
+    if (k <= count_of_odd)
+    {
+        // k-th number is in the odd part
+        cout << 2 * k - 1 << endl;
+    }
+    else
+    {
+        // k-th number is in the even part
+        cout << 2 * (k - count_of_odd) << endl;
+    }
+
+    return 0;
 }
