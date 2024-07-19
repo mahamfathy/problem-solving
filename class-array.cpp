356c50508670aba6ee907a248b5cd20b77c0dbdc
mahamfathy <mahafathyh@gmail.com>
2024-07-19 23:29:39 +0300

    class array

--- class-array.cpp
+++ class-array.cpp
@@ -3,14 +3,33 @@
 using namespace std;
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};                  // c-style array
-    array<int, 8> arr2 = {1, 2, 3, 4, 5, 6, 7, 8}; // class array
-    cout << arr[0] << endl;
-    cout << arr[1] << endl;
-    cout << arr[2] << endl;
-    cout << arr[3] << endl;
-    cout << arr[4] << endl;
-    cout << endl;
-    cout << arr2.size() << endl;
+    // int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8}; // c-style array
+    array<int, 5> arr2 = {1, 2, 3, 4, 5}; // class array
+    cout << "Size before fill: " << arr2.size() << endl;
+
+    cout << arr2[0] << endl;
+    cout << arr2[1] << endl;
+    cout << arr2[2] << endl;
+    cout << arr2[3] << endl;
+    cout << arr2[4] << endl;
+
+    arr2.fill(5);
+    cout << arr2[0] << endl;
+    cout << arr2[1] << endl;
+    cout << arr2[2] << endl;
+    cout << arr2[3] << endl;
+    cout << arr2[4] << endl;
+    arr2.fill('a'); // it will put ascii value
+    cout << arr2[0] << endl;
+    cout << arr2[1] << endl;
+    cout << arr2[2] << endl;
+    cout << arr2[3] << endl;
+    cout << arr2[4] << endl;
+    arr2.fill(true);
+    cout << arr2[0] << endl;
+    cout << arr2[1] << endl;
+    cout << arr2[2] << endl;
+    cout << arr2[3] << endl;
+    cout << arr2[4] << endl;
     return 0;
 }
