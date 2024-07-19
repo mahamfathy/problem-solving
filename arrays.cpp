1033a0d3811962ed8ede41d2898ed51916b15271
mahamfathy <mahafathyh@gmail.com>
2024-07-19 16:03:29 +0300

    arrays examples

--- arrays.cpp
+++ arrays.cpp
@@ -16,5 +16,13 @@ int main()
 
     int all[3];
     cout << all[0] << " " << all[2] << " " << all[5] << endl;
+    cout << endl;
+    //    Two Dimensional Array  //
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << arr[2][2] << endl;
+    const int rows = 3;
+    int cols = 3;
+    int arr2[rows][cols] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << arr2[0][2] << endl;
     return 0;
 }
