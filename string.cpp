a36a5029d674b0ac6fef7f30683e38cae084621d
mahamfathy <mahafathyh@gmail.com>
2024-07-21 23:47:55 +0300

    another examples

--- string.cpp
+++ string.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 int main()
 {
-    string s;
-    cin >> s;
-    cout << s.size() << endl;
+    // string s;
+    // cin >> s;
+    // cout << s.size() << endl;
+    string name_a[] = {"apple", "banana", "cherry"};
+    char name_b[] = "Elzero";
+    cout << name_b << endl
+         << sizeof(name_b) << endl;
+    cout << name_a[0] << endl;
     return 0;
 }
