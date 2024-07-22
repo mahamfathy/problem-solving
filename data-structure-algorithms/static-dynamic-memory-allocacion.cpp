de978a0c153ec6fa7549d0680f83af01a7b9d785
mahamfathy <mahafathyh@gmail.com>
2024-07-22 23:03:59 +0300

    static and dynamic memory allocation

---
+++ data-structure-algorithms/static-dynamic-memory-allocacion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    // int arr[size]; so wrong
+    int *arr = new int[size];
+    cout << arr[size] << endl;
+    return 0;
+}
