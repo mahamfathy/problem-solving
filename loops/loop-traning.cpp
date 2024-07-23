211c566dfe9c00963064b7439410153966ab3dbf
mahamfathy <mahafathyh@gmail.com>
2024-07-23 18:04:12 +0300

    finish reversed elements in the loop training

--- loops/loop-traning.cpp
+++ loops/loop-traning.cpp
@@ -3,62 +3,81 @@ using namespace std;
 
 int main()
 {
-    // 1- count even and positive numbers in an array //
+    // // 1- count even and positive numbers in an array //
 
-    int nums[] = {10, 20, -20, 13, 30, -30, 40};
-    int result = 0;
-    // int numsSize = sizeof(nums) / sizeof(nums[0]); // or
-    int numsSize = size(nums);
-    for (int i = 0; i < numsSize; i++)
-    {
-        if (nums[i] % 2 == 0 && nums[i] > 0)
-        {
-            result += nums[i];
-        }
-    }
-    cout << "result is: " << result << endl;
+    // int nums[] = {10, 20, -20, 13, 30, -30, 40};
+    // int result = 0;
+    // // int numsSize = sizeof(nums) / sizeof(nums[0]); // or
+    // int numsSize = size(nums);
+    // for (int i = 0; i < numsSize; i++)
+    // {
+    //     if (nums[i] % 2 == 0 && nums[i] > 0)
+    //     {
+    //         result += nums[i];
+    //     }
+    // }
+    // cout << "result is: " << result << endl;
 
-    // 2- guess the number //
-    int guessNumber = 7;
-    int guessTries = 0;
-    cout << "guess the number from 1 to 10: " << endl;
-    int number;
+    // // 2- guess the number //
+    // int guessNumber = 7;
+    // int guessTries = 0;
+    // cout << "guess the number from 1 to 10: " << endl;
+    // int number;
 
-    // while (guessTries < 3 && number != guessNumber)
+    // // while (guessTries < 3 && number != guessNumber)
+    // // {
+    // //     cin >> number;
+
+    // //     guessTries++;
+    // //     if (number == guessNumber)
+    // //     {
+    // //         cout << "you win" << endl;
+    // //         continue;
+    // //     }
+
+    // //     cout << "try again: " << endl;
+    // // }
+
+    // // or //
+
+    // while (true)
     // {
     //     cin >> number;
-
-    //     guessTries++;
     //     if (number == guessNumber)
     //     {
     //         cout << "you win" << endl;
-    //         continue;
+    //         break;
     //     }
+    //     else
+    //     {
+    //         cout << "try again: " << endl;
+    //         guessTries++;
+    //     }
+    //     if (guessTries == 3)
+    //     {
+    //         cout << "sorry, you lose" << endl;
 
-    //     cout << "try again: " << endl;
+    //         break;
+    //     }
     // }
 
-    // or
-    while (true)
-    {
-        cin >> number;
-        if (number == guessNumber)
-        {
-            cout << "you win" << endl;
-            break;
-        }
-        else
-        {
-            cout << "try again: " << endl;
-            guessTries++;
-        }
-        if (guessTries == 3)
-        {
-            cout << "sorry, you lose" << endl;
+    // 3- reversed elements from user //
 
-            break;
-        }
+    int vals[5];
+    int input;
+    cout << "enter 5 numbers: " << endl;
+    int valSize = size(vals);
+    for (int i = 0; i < valSize; i++)
+    {
+        cin >> input;
+        vals[i] = input;
     }
+    cout << "================\n";
+    for (int i = valSize - 1; i >= 0; i--)
+    {
+        cout << vals[i] << endl;
+    }
+    // or i can do the in for input above
 
     return 0;
 }
