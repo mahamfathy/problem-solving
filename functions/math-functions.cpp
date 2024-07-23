83c322973dad065ff64d5547579c3909d3ecdaa7
mahamfathy <mahafathyh@gmail.com>
2024-07-23 23:27:49 +0300

    math build-in functions examples

---
+++ functions/math-functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include <math.h>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout << pow(2, 4) << endl;
+    cout << sqrt(16) << endl;
+    cout << ceil(5.2) << endl;
+    cout << floor(5.5) << endl;
+    cout << round(5.5) << endl;
+    cout << round(5.3) << endl;
+    cout << fmod(11.5, 2) << endl;
+    cout << trunc(1.5) << endl;
+    cout << trunc(1.1) << endl;
+    cout << trunc(1.9) << endl;
+
+    return 0;
+}
