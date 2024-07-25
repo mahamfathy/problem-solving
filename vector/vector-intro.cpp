f81d7191b9c44d0b31352504f9f3eab7eb5cedad
mahamfathy <mahafathyh@gmail.com>
2024-07-26 02:38:21 +0300

    finish vector explaination

--- vector/vector-intro.cpp
+++ vector/vector-intro.cpp
@@ -5,21 +5,41 @@ using namespace std;
 
 int main()
 {
+
     vector<int> numsOne = {10, 50, 60, 40};
     vector<int> numsTwo{10, 20, 30, 40, 50};
     vector<int> numsThree(4, 50);
+
     for (int i = 0; i < numsOne.size(); i++)
     {
         // cout << numsOne[i] << endl;
         cout << numsOne.at(i) << "  ";
     }
 
-    cout << "\n=================\n";
+    cout << "\n==================\n";
 
     for (int i = 0; i < numsTwo.size(); i++)
     {
         cout << numsTwo.at(i) << "  ";
     }
+    cout << "\n==================\n";
+    for (int i = 0; i < numsThree.size(); i++)
+    {
+        cout << numsThree.at(i) << "  ";
+    }
+    cout << "\n==================\n";
 
+    cout << numsThree.size() << endl;
+
+    // numsThree.at(4) = 1000; wrong to push any value
+    numsThree.at(0) = 1000;
+    numsThree.push_back(1000);
+    cout << numsThree.size();
+    cout << "\n==================\n";
+
+    for (int i = 0; i < numsThree.size(); i++)
+    {
+        cout << numsThree.at(i) << "  ";
+    }
     return 0;
 }
