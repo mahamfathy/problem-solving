100336dd8aa080a5300123ac373558baac922306
mahamfathy <mahafathyh@gmail.com>
2024-08-13 21:15:34 +0300

    qwerty task

---
+++ task-day3/qwerty .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // cin >> n;
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+    int a = 10;
+    cout << int(a) << endl;
+
+    return 0;
+}
