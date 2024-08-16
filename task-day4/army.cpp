cc1556eb75014fc8fccc804b64a31c83471e3181
mahamfathy <mahafathyh@gmail.com>
2024-08-17 00:25:56 +0300

    army solution

---
+++ task-day4/army.cpp
@@ -0,0 +1,22 @@
+#include <iostream>>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int d[n - 1];
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> d[i];
+    }
+    int a, b;
+    cin >> a >> b;
+    int totalYears = 0;
+    for (int i = a - 1; i < b - 1; i++)
+    {
+        totalYears += d[i];
+    }
+    cout << totalYears << endl;
+    return 0;
+}
--- task-day4/maximum-in-table.cpp
+++ task-day4/maximum-in-table.cpp
@@ -25,21 +25,19 @@ int main()
 {
     int n;
     cin >> n;
-    int a[n][n];
-    for (int i = 0; i < n; ++i)
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
     {
-        a[0][i] = 1;
-        a[i][0] = 1;
+        arr[0][i] = 1;
+        arr[i][0] = 1;
     }
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 1; j < n; ++j)
+        for (int j = 1; j < n; j++)
         {
-            a[i][j] = a[i - 1][j] + a[i][j - 1];
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
         }
     }
-
-    cout << a[n - 1][n - 1] << endl;
-
+    cout << arr[n - 1][n - 1] << endl;
     return 0;
 }
