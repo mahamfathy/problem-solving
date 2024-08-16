cf1f8408711e3bbdc2aa537f084225b5e7aaa0a9
mahamfathy <mahafathyh@gmail.com>
2024-08-17 00:50:54 +0300

    beautiful matrix solution

---
+++ task-day4/beautiful-matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int matrix[5][5];
+    int x, y;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1)
+            {
+                x = i + 1;
+                y = j + 1;
+            }
+        }
+    }
+    int moves = abs(x - 3) + abs(y - 3);
+    cout << moves << endl;
+    return 0;
+}
