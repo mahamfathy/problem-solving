f61f7fdfe478fa4e419339d965e8825a7e68cd74
mahamfathy <mahafathyh@gmail.com>
2024-08-16 23:39:01 +0300

    rint-sub array solution

--- task-day4/arrays.cpp
+++ task-day4/arrays.cpp
@@ -1,46 +1,5 @@
 
 
-// #include <iostream>
-// using namespace std;
-
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     double sum = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> arr[i];
-//         sum += arr[i];
-//     }
-//     double average = sum / n;
-//     cout << average << endl;
-
-//     return 0;
-// }
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     cin >> n;
-
-//     int A[n];
-//     for (int i = 0; i < n; ++i) {
-//         cin >> A[i];
-//     }
-
-//     int l, r;
-//     cin >> l >> r;
-
-//     for (int i = l - 1; i <= r - 1; ++i) {
-//         cout << A[i] << " ";
-//     }
-//     cout << endl;
-
-//     return 0;
-// }
 #include <iostream>
 using namespace std;
 
