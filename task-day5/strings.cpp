8af990bd1f12d83837963b79b7b2262363d0d402
mahamfathy <mahafathyh@gmail.com>
2024-09-09 21:14:50 +0300

    concatinating

---
+++ task-day5/strings.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    // char str[50];
+    // cin >> str;
+    // char str[50] = "hello world";
+    // char out[50];
+    // strcpy(out, str);
+    // cout << out << endl;
+    // cout << strlen(str) << endl;
+    // cout << str << endl;
+    char str1[] = "Hello ";
+    char str2[] = "World";
+    // int compare = strcmp(str1, str2);
+    // cout << compare << endl;
+    // if (compare == 0)
+    //     cout << "equal" << endl;
+
+    // else if (compare > 0)
+    //     cout << "str1 is greater" << endl;
+
+    // else
+    //     cout << "str2 is greater" << endl;
+    // strcat(str1, str2);
+    // cout << str1 << endl;
+    // cout << strncat(str1, str2, 3) << endl;
+    return 0;
+}
