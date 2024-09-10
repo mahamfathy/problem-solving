516da605ae5d364b602b6e7b026b65eafa9f87e5
mahamfathy <mahafathyh@gmail.com>
2024-09-10 17:35:28 +0300

    h issue

---
+++ task-day5/h.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '9' && i == 0)
+            continue;
+        if ('9' - s[i] < s[i] - '0')
+            s[i] = '9' - s[i] + '0';
+    }
+    cout << s << endl;
+    return 0;
+}
