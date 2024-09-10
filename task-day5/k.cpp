ef5e4e18f4cc4ac9bc6ccf753ea69290fbd894a5
mahamfathy <mahafathyh@gmail.com>
2024-09-10 18:52:41 +0300

    k issue

---
+++ task-day5/k.cpp
@@ -0,0 +1,54 @@
+// #include "iostream"
+// #include "algorithm"
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     string s;
+//     cin >> s;
+//     bool arr[26] = {0};
+//     for (int i = 0; i < s.size(); i++)
+//     {
+//         arr[tolower(s[i]) - 'a'] = 1;
+//         bool flag = true;
+//         for (int i = 0; i < 26; i++)
+//         {
+//             if (!arr[i])
+//             {
+//                 flag = false;
+//                 break;
+//             }
+//         }
+
+//         cout << (flag ? "YES" : "No") << '\n';
+//     }
+//     return 0;
+// }
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool arr[26] = {0};
+    for (int i = 0; i < s.size(); i++)
+        arr[tolower(s[i]) - 'a'] = 1;
+    bool flag = true;
+    for (int i = 0; i < 26; i++)
+    {
+        if (!arr[i])
+        {
+            flag = false;
+            break;
+        }
+    }
+    cout << (flag ? "YES" : "NO") << '\n';
+    return 0;
+}
