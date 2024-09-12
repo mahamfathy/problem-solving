6c4474a71c82a60e7541bd20a4e1ec9395105977
mahamfathy <mahafathyh@gmail.com>
2024-09-12 20:29:16 +0300

    first solution of tram

--- task-day6/tram-i.cpp
+++ task-day6/tram-i.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main()
 {
-int n,curr=0,ans=0;
-cin>>n;
-while(n--){
-    int a,b;
-    cin>>a>>b;
-    ans=max(ans,curr);
-curr-=a;
-curr+=b;
-}
-co
+    int n, curr = 0, ans = 0;
+    cin >> n;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        ans = max(ans, curr);
+        curr -= a;
+        curr += b;
+    }
+    cout << ans << endl;
     return 0;
 }
