f81601085ddac94f4af1b3f8639ee414800fd2b3
mahamfathy <mahafathyh@gmail.com>
2024-09-12 22:54:28 +0300

    tram solution

--- task-day6/tram-i.cpp
+++ task-day6/tram-i.cpp
@@ -22,16 +22,31 @@ using namespace std;
 int main()
 {
     int n;
+
+    cin >> n;
     int arr[n], a[n], b[n];
     for (int i = 0; i < n; i++)
         cin >> a[i] >> b[i];
     arr[0] = b[0];
     int ans = arr[0];
-     for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        arr[i] = (arr[i] - a[i]) + b[i];
+        arr[i] = (arr[i - 1] - a[i]) + b[i];
         ans = max(ans, arr[i]);
     }
+    // int n;
+    // cin >> n;
+    // int arr[n], a[n], b[n];
+    // int ans = 0;
+    // for (int i = 0; i < n; i++)
+    //     cin >> a[i] >> b[i];
+    // arr[0] = a[0] + b[0];
+    // ans = arr[0];
+    // for (int i = 1; i < n; i++)
+    // {
+    //     arr[i] = (arr[i - 1] - a[i]) + b[i];
+    //     ans = max(ans, arr[i]);
+    // }
     cout << ans << endl;
     return 0;
 }
