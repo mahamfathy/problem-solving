520b2628710a6225e479353b5d1ad63fa44c69a7
mahamfathy <mahafathyh@gmail.com>
2024-09-12 12:39:41 +0300

    second solution for currency

--- task-day6/currency-system-b.cpp
+++ task-day6/currency-system-b.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+//     cout << ((count(arr, arr + n, 1) > 0) ? -1 : 1) << endl;
+//     return 0;
+// }
+
 int main()
 {
     int n;
     cin >> n;
     int arr[n];
+    int mn = 2;
     for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
-    cout << ((count(arr, arr + n, 1) > 0) ? -1 : 1) << endl;
+        mn = min(mn, arr[i]);
+    }
+    cout << (mn == 1 ? -1 : 1) << '\n';
     return 0;
 }
