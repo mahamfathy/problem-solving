2daac63bc37751d2316ed31ec4055026c1b3a979
mahamfathy <mahafathyh@gmail.com>
2024-09-12 20:50:00 +0300

    second solution of tram problem

--- task-day6/tram-i.cpp
+++ task-day6/tram-i.cpp
@@ -1,17 +1,36 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n, curr = 0, ans = 0;
+//     cin >> n;
+//     while (n--)
+//     {
+//         int a, b;
+//         cin >> a >> b;
+//         ans = max(ans, curr);
+//         curr -= a;
+//         curr += b;
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int n, curr = 0, ans = 0;
-    cin >> n;
-    while (n--)
+    int n;
+    int arr[n], a[n], b[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i] >> b[i];
+    arr[0] = b[0];
+    int ans = arr[0];
+     for (int i = 1; i < n; i++)
     {
-        int a, b;
-        cin >> a >> b;
-        ans = max(ans, curr);
-        curr -= a;
-        curr += b;
+        arr[i] = (arr[i] - a[i]) + b[i];
+        ans = max(ans, arr[i]);
     }
     cout << ans << endl;
     return 0;
