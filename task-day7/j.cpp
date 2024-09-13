efb878866b09ccaa007b8632c8bb92f274f68c78
mahamfathy <mahafathyh@gmail.com>
2024-09-13 20:55:51 +0300

    j solution

---
+++ task-day7/j.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+int arr[N];
+int main()
+{
+    int n;
+    cin >> n;
+    long long sum = 0, x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cin >> x;
+    long long rep = x / sum;
+    sum *= rep;
+    long long idx = rep * n;
+    for (int i = 0; i < n; i++)
+    {
+        if (sum > x)
+            break;
+        sum += arr[i];
+        idx++;
+    }
+    cout << idx << endl;
+    return 0;
+}
