01088beb4a4bfdf3358ae0f2432b34eb213b7da8
mahamfathy <mahafathyh@gmail.com>
2024-09-13 19:24:20 +0300

    e solution

--- task-day7/e.cpp
+++ task-day7/e.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cin >> a >> b >> c;
-    swap(a, b);
-    swap(a, c);
+    // cin >> a >> b >> c;
+    // swap(a, b);
+    // swap(a, c);
+    cin >> b >> c >> a;
     cout << a << " " << b << " " << c << endl;
     return 0;
 }
