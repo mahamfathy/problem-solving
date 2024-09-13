521db9f0160af7383ca80514706d6e79aae97cf0
mahamfathy <mahafathyh@gmail.com>
2024-09-13 19:48:50 +0300

    f problem

--- task-day7/f.cpp
+++ task-day7/f.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main()
 {
-
+    int n;
+    string s;
+    cin >> n >> s;
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+        cnt += s[i] == s[i + 1];
+    // if (s[i] == s[i + 1])
+    //     cnt++;
+    cout << cnt << endl;
     return 0;
 }
