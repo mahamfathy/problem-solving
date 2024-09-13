0b9f6ffa572e7071edec983a1ce634297bb62c61
mahamfathy <mahafathyh@gmail.com>
2024-09-13 18:56:05 +0300

    d problem

--- task-day7/d.cpp
+++ task-day7/d.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    int n;
+    long long n;
     cin >> n;
     if (n % 2 == 0)
-        cout << n/2 << endl;
+        cout << n / 2 << endl;
     else
-        cout << -((n+1) /2)  << endl;
+        cout << -((n + 1) / 2) << endl;
     return 0;
 }
