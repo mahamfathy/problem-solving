4e49f338a776041b33979d6ad527fde9f764d918
mahamfathy <mahafathyh@gmail.com>
2024-09-14 17:19:10 +0300

    ada deque

---
+++ task-day8/G -ADA & QUEUE.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int q;
+    cin >> q;
+    bool rev = false;
+    deque<int> deq;
+    while (q--)
+    {
+        string s;
+        cin >> s;
+        if (s == "push_back")
+        {
+            int x;
+            cin >> x;
+            if (!rev)
+                deq.push_back(x);
+            else
+                deq.push_front(x);
+        }
+        if (s == "toFront")
+        {
+            int x;
+            cin >> x;
+            if (!rev)
+                deq.push_front(x);
+            else
+                deq.push_back(x);
+        }
+
+        if (s == "front")
+        {
+            if (deq.empty())
+            {
+                cout << "No job for Ada?" << '\n';
+                continue;
+            }
+
+            if (!rev)
+            {
+                cout << deq.front() << '\n';
+                deq.pop_front();
+            }
+            else
+            {
+                cout << deq.back() << '\n';
+                deq.pop_back();
+            }
+        }
+        if (s == "back")
+        {
+            if (deq.empty())
+            {
+                cout << "No job for Ada?" << '\n';
+                continue;
+            }
+                            if (!rev)
+                {
+                    cout << deq.back() << '\n';
+                    deq.pop_back();
+                }
+                else
+                {
+                    cout << deq.front() << '\n';
+                    deq.pop_front();
+               
+            }
+        }
+         if (s == "reverse")
+            rev = !rev;
+    }
+    return 0;
+}
