ce8bd6b41145efe66eedf301ac390031ef8cfaf8
mahamfathy <mahafathyh@gmail.com>
2024-10-03 21:19:35 +0300

    finish a solution

--- task-day11/a-fibonacci.cpp
+++ task-day11/a-fibonacci.cpp
@@ -1,11 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-int super_digit(int n, int k)
+
+int fib(int n)
 {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
     return 0;
 }
