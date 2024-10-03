2bc85c99e453bbb1bb773c34c37d5fa9f8865e0d
mahamfathy <mahafathyh@gmail.com>
2024-10-03 23:17:46 +0300

    solution of c

--- task-day11/c-Recursive Digit Sum.cpp
+++ task-day11/c-Recursive Digit Sum.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+int sum(string s)
+{
+    int res = 0;
+    for (auto i : s)
+        res += (i - '0');
+    // res += stoi(i);
+    return res;
+}
 int super_digit(string s)
 {
-    int sum = 0;
-        for (i : s)
-            sum += (i - '0');
-return sum;
-       
-    }
+    if (s.size() == 1)
+        // return sum(s);
+        return stoi(s);
+    return super_digit(to_string(sum(s)));
 }
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     string n;
     int k;
-    cin >> s >> n;
-    cout << super_digit(s) * k << endl;
+    cin >> n >> k;
+    int ans = super_digit(n) * k;
+    cout << super_digit(to_string(ans)) << endl;
     return 0;
 }
