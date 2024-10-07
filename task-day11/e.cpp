dc38deeb42ff1591c102dd0f6310836c817d280b
mahamfathy <mahafathyh@gmail.com>
2024-10-08 00:59:10 +0300

    e solution

--- task-day11/e.cpp
+++ task-day11/e.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-int damage(int x, int y)
+int damage(int s, int d, int x, int y)
 {
-    if (x == 0 && y == 0)
+
+    if (x < s && y > d)
+        return 1;
+    if (x >= s || y <= d)
         return 0;
-    else
-        return;
+    return damage(s, d, x + 1, y - 1);
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int n, s, d;
+    int n, s, d, x, y;
+    bool damage_found = false;
     cin >> n >> s >> d;
     while (n--)
     {
-        int x, y;
         cin >> x >> y;
+        if (damage(s, d, x, y))
+            damage_found = true;
     }
+    cout << (damage_found ? "Yes" : "No") << endl;
     return 0;
 }
